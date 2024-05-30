9602a79ba04f39e290e6605b1908ad4242d4c97c
Rafael França <rafaelfranca@live.com>
2024-05-30 19:58:38 -0300

    react-03-dt-money

---
+++ README.md
@@ -0,0 +1,15 @@
+# DT Money
+
+Projeto de aula da RocketSeat da jornada de React. Projeto [03]
+
+Orientaçoes:
+
+- [Figma DT Money](react-03-dt-money)
+
+## Dependências [npm install]
+
+- styled-components
+- @types/styled-components -D
+
+### como usar ?
+
---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
+import { Transactions } from "./pages/Transactions";
+
+export function App() {
+
+
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle/>
+
+      <Transactions/>
+    <h1>
+     Hello world
+    </h1>
+    </ThemeProvider>
+  )
+}
